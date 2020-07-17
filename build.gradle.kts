90295122e2142926d583b399d06fdae225b9d583
Ryan Batchelder <ryanbatch@gmail.com>
2020-07-17 15:25:43 -0400

    Revert: Update deploy registry URL

--- build.gradle.kts
+++ build.gradle.kts
@@ -48,7 +48,7 @@ publishing {
 
         maven {
             name = "GitHubPackages"
-            url = uri("https://maven.pkg.github.com/rdbatch02")
+            url = uri("https://maven.pkg.github.com/rdbatch02/cfn-custom-resource-bootstrap")
             credentials {
                 username = System.getenv("GITHUB_ACTOR")
                 password = System.getenv("GITHUB_TOKEN")
